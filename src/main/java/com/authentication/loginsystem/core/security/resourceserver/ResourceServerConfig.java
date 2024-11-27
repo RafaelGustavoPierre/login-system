0684c20d80f281c089515766fe84b5fe638aec35
RafaelGustavoPierre <rafaelptjr@gmail.com>
2024-11-27 20:52:48 -0300

    :sparkles: implementing customer consent revocation

--- src/main/java/com/authentication/loginsystem/core/security/resourceserver/ResourceServerConfig.java
+++ src/main/java/com/authentication/loginsystem/core/security/resourceserver/ResourceServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,7 +17,7 @@ public class ResourceServerConfig {
     @Bean
     public SecurityFilterChain resourceFilterChain(HttpSecurity http) throws Exception {
         http.formLogin(Customizer.withDefaults());
-
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.formLogin(customizer -> customizer.loginPage("/login")).build();
     }
 
